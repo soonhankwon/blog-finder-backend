4487302cef99a592fd9915e1486cca4b031cc476
soonable@naver.com <soonable@naver.com>
2023-03-24 13:26:37 +0900

    test : Enum 클래스 sortType value equals 테스트

--- blog-finder-core/src/test/java/domain/SortTypeTest.java
+++ blog-finder-core/src/test/java/domain/SortTypeTest.java
@@ -1,16 +1,19 @@
 package domain;
 
 import com.soon.domain.SortType;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SortTypeTest {
 
     @Test
     @DisplayName("SortType value equals 테스트")
     void enumValueEqualsString() {
-        String str = SortType.ACCURACY.getValue();
-        Assertions.assertThat(str).isEqualTo("accuracy");
+        String str1 = SortType.ACCURACY.getValue();
+        String str2 = SortType.RECENCY.getValue();
+        assertThat(str1).isEqualTo("accuracy");
+        assertThat(str2).isEqualTo("recency");
     }
 }
