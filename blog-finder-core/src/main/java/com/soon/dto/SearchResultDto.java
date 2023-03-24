475c3f2f1c88a2d27035ea89bebf1110b92af583
soonable@naver.com <soonable@naver.com>
2023-03-24 10:20:50 +0900

    feat : 검색 응답결과 Dto Builder 패턴

--- blog-finder-core/src/main/java/com/soon/dto/SearchResultDto.java
+++ blog-finder-core/src/main/java/com/soon/dto/SearchResultDto.java
@@ -1,4 +1,17 @@
 package com.soon.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
 public class SearchResultDto {
+    private String blogname;
+    private String contents;
+    private String title;
+    private String url;
+    private LocalDateTime datetime;
+    private String thumbnail;
 }
