08a1ce6a0553af265ba2eefae7165c4a70b99697
soonable@naver.com <soonable@naver.com>
2023-03-24 13:26:00 +0900

    bug : jackson json parsing 시 dto 에 getter 필요

--- blog-finder-core/src/main/java/com/soon/dto/SearchResultDto.java
+++ blog-finder-core/src/main/java/com/soon/dto/SearchResultDto.java
@@ -1,17 +1,25 @@
 package com.soon.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Builder
+@Getter
 @AllArgsConstructor
 public class SearchResultDto {
     private String blogname;
-    private String contents;
+
     private String title;
+
+    private String contents;
+
     private String url;
+
     private LocalDateTime datetime;
+
     private String thumbnail;
 }
