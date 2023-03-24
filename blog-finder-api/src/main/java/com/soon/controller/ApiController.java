d9f0257c90e0127087e931a3a07b1f148ca79f22
soonable@naver.com <soonable@naver.com>
2023-03-24 10:45:30 +0900

    feat : 카카오 키워드 블로그 검색 api

--- blog-finder-api/src/main/java/com/soon/controller/ApiController.java
+++ blog-finder-api/src/main/java/com/soon/controller/ApiController.java
@@ -1,12 +1,32 @@
 package com.soon.controller;
 
+import com.soon.dto.SearchResultDto;
+import com.soon.service.KeywordSearchServiceRouter;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@Tag(name = "카카오 & 네이버 API 키워드 블로그 검색 API")
+@Tag(name = "카카오 블로그 검색 API")
 public class ApiController {
+    private final KeywordSearchServiceRouter keywordSearchServiceRouter;
 
+    @GetMapping("/search/accuracy")
+    @Operation(summary = "정확도순 블로그 검색 API")
+    public Mono<List<SearchResultDto>> apiSearchAccuracy(@RequestParam String query, String sortType) {
+        return keywordSearchServiceRouter.searchByKakao(query, sortType);
+    }
+
+    @GetMapping("/search/recency")
+    @Operation(summary = "최신순 블로그 검색 API")
+    public Mono<List<SearchResultDto>> apiSearchRecency(@RequestParam String query, String sortType) {
+        return keywordSearchServiceRouter.searchByKakao(query, sortType);
+    }
 }
