19977f155451ccd63d746e3c4263c91b33570512
soonable@naver.com <soonable@naver.com>
2023-03-26 19:52:16 +0900

    feat : 서킷브레이커 적용 카카오 에러율 60% -> 네이버 Fallback & 10Min Half-open

--- blog-finder-core/src/main/java/com/soon/service/KeywordSearchServiceRouter.java
+++ blog-finder-core/src/main/java/com/soon/service/KeywordSearchServiceRouter.java
@@ -4,6 +4,7 @@ import com.soon.domain.SortType;
 import com.soon.dto.SearchResultDto;
 import com.soon.exception.ErrorCode;
 import com.soon.exception.RequestException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -15,17 +16,19 @@ import java.util.List;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class KeywordSearchServiceRouter implements SearchServiceRouter<Mono<List<SearchResultDto>>>{
+public class KeywordSearchServiceRouter implements SearchServiceRouter<Mono<List<SearchResultDto>>> {
 
     private final KeywordSearchService keywordSearchService;
+    private static final String BREAKER = "breaker";
 
     @Override
+    @CircuitBreaker(name = BREAKER, fallbackMethod = "searchByNaver")
     @Transactional(readOnly = true)
     public Mono<List<SearchResultDto>> searchByKakao(String query, String sortType) {
-        if(sortType.equals(SortType.ACCURACY.getValue())) {
+        if (sortType.equals(SortType.ACCURACY.getValue())) {
             return keywordSearchService.searchByAccuracy(query, sortType);
         }
-        if(sortType.equals(SortType.RECENCY.getValue())) {
+        if (sortType.equals(SortType.RECENCY.getValue())) {
             return keywordSearchService.searchByRecency(query, sortType);
         } else {
             throw new RequestException(ErrorCode.SORT_TYPE_INVALID);
@@ -34,12 +37,12 @@ public class KeywordSearchServiceRouter implements SearchServiceRouter<Mono<List
 
     @Override
     @Transactional(readOnly = true)
-    public Mono<List<SearchResultDto>> searchByNaver(String query, String sortType) {
-        if(sortType.equals(SortType.SIM.getValue())) {
-            return keywordSearchService.searchByAccuracy(query, sortType);
+    public Mono<List<SearchResultDto>> searchByNaver(String query, String sortType, RuntimeException e) {
+        if (sortType.equals(SortType.ACCURACY.getValue())) {
+            return keywordSearchService.searchByAccuracy(query, "sim");
         }
-        if(sortType.equals(SortType.DATE.getValue())) {
-            return keywordSearchService.searchByRecency(query, sortType);
+        if (sortType.equals(SortType.RECENCY.getValue())) {
+            return keywordSearchService.searchByRecency(query, "date");
         } else {
             throw new RequestException(ErrorCode.SORT_TYPE_INVALID);
         }
--- blog-finder-search/src/main/java/com/soon/service/SearchServiceRouter.java
+++ blog-finder-search/src/main/java/com/soon/service/SearchServiceRouter.java
@@ -2,5 +2,5 @@ package com.soon.service;
 
 public interface SearchServiceRouter<T> {
     T searchByKakao(String query, String sortType);
-    T searchByNaver(String query, String sortType);
+    T searchByNaver(String query, String sortType, RuntimeException e);
 }
