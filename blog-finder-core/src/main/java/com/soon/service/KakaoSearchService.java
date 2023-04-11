6d5f6fe36ecd56a23490adb371dd5c6584f954d3
soonable@naver.com <soonable@naver.com>
2023-04-11 21:16:05 +0900

    refactor : 카카오, 네이버 검색 서비스 클래스분리

---
+++ blog-finder-core/src/main/java/com/soon/service/KakaoSearchService.java
@@ -0,0 +1,60 @@
+package com.soon.service;
+
+import com.soon.domain.SortType;
+import com.soon.dto.SearchResultDto;
+import com.soon.exception.ErrorCode;
+import com.soon.exception.RequestException;
+import com.soon.utils.ApiReqValueStorage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+@RequiredArgsConstructor
+@Service
+public class KakaoSearchService implements SearchService<Mono<List<SearchResultDto>>>  {
+    private final ApiReqValueStorage apiReqValueStorage;
+
+    @Override
+    public Mono<List<SearchResultDto>> search(String query, String sortType) {
+        if (sortType.equals(SortType.ACCURACY.getValue())) {
+            return searchResultToMono(query, SortType.ACCURACY);
+        }
+        if (sortType.equals(SortType.RECENCY.getValue())) {
+            return searchResultToMono(query, SortType.RECENCY);
+        } else {
+            throw new RequestException(ErrorCode.SORT_TYPE_INVALID);
+        }
+    }
+
+    private Mono<List<SearchResultDto>> searchResultToMono(String query, SortType sortType) {
+        return WebClient.builder()
+                .baseUrl(apiReqValueStorage.getKakaoUrl())
+                .build().get()
+                .uri(builder -> builder.path(apiReqValueStorage.getKakaoPath())
+                        .queryParam("query", query)
+                        .queryParam("sort", sortType.getValue())
+                        .queryParam("page", apiReqValueStorage.getKakaoPagination())
+                        .build())
+                .header("Authorization", "KakaoAK " + apiReqValueStorage.getKakaoKey())
+                .retrieve()
+                .bodyToMono(Map.class)
+                .map(map -> (List<Map>) map.get("documents"))
+                .flatMapIterable(Function.identity())
+                .map(document -> SearchResultDto.builder()
+                        .blogname(document.get("blogname").toString())
+                        .title(document.get("title").toString())
+                        .contents(document.get("contents").toString())
+                        .thumbnail(document.get("thumbnail").toString())
+                        .datetime(LocalDateTime.parse(document.get("datetime").toString(), DateTimeFormatter.ISO_DATE_TIME))
+                        .url(document.get("url").toString())
+                        .build())
+                .collectList();
+    }
+}
---
+++ blog-finder-core/src/main/java/com/soon/service/NaverSearchService.java
@@ -0,0 +1,60 @@
+package com.soon.service;
+
+import com.soon.domain.SortType;
+import com.soon.dto.SearchResultDto;
+import com.soon.exception.ErrorCode;
+import com.soon.exception.RequestException;
+import com.soon.utils.ApiReqValueStorage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+@RequiredArgsConstructor
+@Service
+public class NaverSearchService implements SearchService<Mono<List<SearchResultDto>>> {
+    private final ApiReqValueStorage apiReqValueStorage;
+
+    @Override
+    public Mono<List<SearchResultDto>> search(String query, String sortType) {
+        if (sortType.equals(SortType.SIM.getValue())) {
+            return searchResultToMono(query, SortType.SIM);
+        }
+        if (sortType.equals(SortType.DATE.getValue())) {
+            return searchResultToMono(query, SortType.DATE);
+        } else {
+            throw new RequestException(ErrorCode.SORT_TYPE_INVALID);
+        }
+    }
+
+    private Mono<List<SearchResultDto>> searchResultToMono(String query, SortType sortType) {
+        return WebClient.builder()
+                .baseUrl(apiReqValueStorage.getNaverUrl())
+                .build().get()
+                .uri(uriBuilder -> uriBuilder.path(apiReqValueStorage.getNaverPath())
+                        .queryParam("query", query)
+                        .queryParam("display", apiReqValueStorage.getNaverDisplay())
+                        .queryParam("sort", sortType.getValue())
+                        .build())
+                .header("X-Naver-Client-Id", apiReqValueStorage.getNaverClientId())
+                .header("X-Naver-Client-Secret", apiReqValueStorage.getNaverClientSecret())
+                .retrieve()
+                .bodyToMono(Map.class)
+                .map(map -> (List<Map>) map.get("items"))
+                .flatMapIterable(Function.identity())
+                .map(item -> SearchResultDto.builder()
+                        .blogname(item.get("bloggername").toString())
+                        .title(item.get("title").toString())
+                        .contents(item.get("description").toString())
+                        .datetime(LocalDate.parse(item.get("postdate").toString(), DateTimeFormatter.ofPattern("yyyyMMdd")).atStartOfDay())
+                        .url(item.get("link").toString())
+                        .build())
+                .collectList();
+    }
+}
