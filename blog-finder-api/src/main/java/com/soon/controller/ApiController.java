853de4a7ce36d011ab5c9fd91300554a0be4d743
soonable@naver.com <soonable@naver.com>
2023-04-17 20:17:05 +0900

    refactor : 매개변수 SortType 오브젝트로 수정

--- blog-finder-api/src/main/java/com/soon/controller/ApiController.java
+++ blog-finder-api/src/main/java/com/soon/controller/ApiController.java
@@ -1,6 +1,7 @@
 package com.soon.controller;
 
 import com.soon.domain.Keyword;
+import com.soon.domain.SortType;
 import com.soon.dto.SearchResultDto;
 import com.soon.event.SearchEvent;
 import com.soon.service.KeywordSearchServiceRouter;
@@ -24,7 +25,7 @@ public class ApiController {
 
     @GetMapping("/search")
     @Operation(summary = "카카오 정확도순 & 최신순 블로그 검색 API")
-    public Mono<List<SearchResultDto>> blogSearchByKeyword(@RequestParam("query") String query, @RequestParam("sortType") String sortType) {
+    public Mono<List<SearchResultDto>> blogSearchByKeyword(@RequestParam("query") String query, @RequestParam("sortType") SortType sortType) {
         applicationEventPublisher.publishEvent(new SearchEvent(this, new Keyword(query)));
         return keywordSearchServiceRouter.searchByKakao(query, sortType);
     }
