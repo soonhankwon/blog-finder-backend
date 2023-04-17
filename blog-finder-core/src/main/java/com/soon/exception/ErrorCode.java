d48e47b79108769cccec5b9786efc4b850cb2355
soonable@naver.com <soonable@naver.com>
2023-04-17 21:11:40 +0900

    feat : 서비스 미구현 커스텀 에러코드

--- blog-finder-core/src/main/java/com/soon/exception/ErrorCode.java
+++ blog-finder-core/src/main/java/com/soon/exception/ErrorCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     SORT_TYPE_INVALID(HttpStatus.BAD_REQUEST, "정렬 방법이 올바르지 않습니다."),
     KEYWORD_INVALID(HttpStatus.BAD_REQUEST, "키워드는 1자리 이상이어야 합니다."),
-    SEARCH_COUNT_INVALID(HttpStatus.BAD_REQUEST, "유효하지 않은 카운트입니다.");
+    SEARCH_COUNT_INVALID(HttpStatus.BAD_REQUEST, "유효하지 않은 카운트입니다."),
+    SERVICE_NOT_FOUND(HttpStatus.NOT_FOUND, "서비스가 구현되어 있지 않습니다." );
 
     private final HttpStatus httpStatus;
     private final String message;
