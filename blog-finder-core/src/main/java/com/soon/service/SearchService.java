98d179ad27d3d60c6598d583bc631895965e973a
soonable@naver.com <soonable@naver.com>
2023-04-17 21:11:02 +0900

    refactor : interface 매개변수 SortType 으로 변경

--- blog-finder-core/src/main/java/com/soon/service/SearchService.java
+++ blog-finder-core/src/main/java/com/soon/service/SearchService.java
@@ -1,10 +1,11 @@
 package com.soon.service;
 
+import com.soon.domain.SortType;
 import com.soon.dto.SearchResultDto;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 public interface SearchService {
-    Mono<List<SearchResultDto>> blogSearchByKeyword(String query, String sortType);
+    Mono<List<SearchResultDto>> blogSearchByKeyword(String query, SortType sortType);
 }
