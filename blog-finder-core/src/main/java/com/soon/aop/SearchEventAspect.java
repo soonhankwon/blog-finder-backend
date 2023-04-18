1fcae704b642218a6e1813d79c579c7e3cffc549
soonable@naver.com <soonable@naver.com>
2023-04-18 18:12:31 +0900

    feat : SearchEvent aop 적용

---
+++ blog-finder-core/src/main/java/com/soon/aop/SearchEventAspect.java
@@ -0,0 +1,22 @@
+package com.soon.aop;
+
+import com.soon.domain.Keyword;
+import com.soon.domain.SortType;
+import com.soon.event.SearchEvent;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@RequiredArgsConstructor
+public class SearchEventAspect {
+    private final ApplicationEventPublisher applicationEventPublisher;
+
+    @Before(value = "execution(* com.soon.controller.ApiController.blogSearchByKeyword(..)) && args(query,sortType)", argNames = "query,sortType")
+    public void beforeBlogSearch(String query, SortType sortType) {
+        applicationEventPublisher.publishEvent(new SearchEvent(this, new Keyword(query)));
+    }
+}
