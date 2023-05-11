9bd3936e4d49c00ae847b52c7be8bef1a95941a6
soonable@naver.com <soonable@naver.com>
2023-05-11 13:40:35 +0900

    refactor : rename & uri(리소스만 표현하도록 수정)

--- blog-finder-api/src/main/java/com/soon/controller/BlogSearchController.java
+++ blog-finder-api/src/main/java/com/soon/controller/BlogSearchController.java
@@ -1,14 +1,11 @@
 package com.soon.controller;
 
-import com.soon.domain.Keyword;
 import com.soon.domain.SortType;
 import com.soon.dto.SearchResultDto;
-import com.soon.event.SearchEvent;
-import com.soon.service.KeywordSearchServiceRouter;
+import com.soon.service.BlogSearchServiceRouter;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,12 +16,12 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @Tag(name = "블로그 검색 API")
-public class ApiController {
-    private final KeywordSearchServiceRouter keywordSearchServiceRouter;
+public class BlogSearchController {
+    private final BlogSearchServiceRouter blogSearchServiceRouter;
 
-    @GetMapping("/search")
+    @GetMapping("/blog")
     @Operation(summary = "카카오 정확도순 & 최신순 블로그 검색 API")
     public Mono<List<SearchResultDto>> blogSearchByKeyword(@RequestParam("query") String query, @RequestParam("sortType") SortType sortType) {
-        return keywordSearchServiceRouter.searchByKakao(query, sortType);
+        return blogSearchServiceRouter.searchByKakao(query, sortType);
     }
 }
